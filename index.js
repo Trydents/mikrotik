8b483217b8077c68ec31dc69177e7b00ef21d59d
jgglcv <jgglcv@gmail.com>
2024-02-17 19:07:37 -0400

    Eliminación de ayudas

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ var MikroNode = require("mikronode2");
 const parseMIkrotikSystem = require("./utilities");
 
 var device = new MikroNode("192.168.4.10");
-var objectTest = { name: "jonatha", apellido: "La Concha" };
-console.log("Objeto de prueba: " + JSON.stringify(objectTest));
 
 device
 	.connect()
@@ -12,13 +10,15 @@ device
 	})
 	.then(function (conn) {
 		console.log("Conexión establecida");
+
 		var chan = conn.openChannel();
+
 		chan.write("/ip/address/print");
 		chan.on("done", function (data) {
 			// data is all of the sentences in an array.
-			console.log("Estos es data:", data.data);
 			data.data.forEach(function (item) {
 				parseMIkrotikSystem(item);
+
 				console.log("Interface/IP: " + item.interface + "/" + item.address);
 			});
 
